d2f17feb766a69f28b4758731db1cb57152e3224
Huming-bird <ahmedolalekan2@gmail.com>
2023-07-06 00:27:59 -0700

    changes to task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,13 +4,14 @@
 sudo apt-get update
 sudo apt-get install nginx
 
-sudo mkdir /data
-sudo mkdir /data/web_static
-sudo mkdir /data/web_static/releases
-sudo mkdir /data/web_static/shared
-sudo mkdir /data/web_static/releases/test
-sudo touch /data/web_static/releases/test/index.html
-if [ -L /data/web_static/current ]; then sudo rm /data/web_static/current; else sudo ln -s /data/web_static/releases/test /data/web_static/current; fi
+if [ ! -d /data ]; then sudo mkdir /data; fi
+if [ ! -d /data/web_static ]; then sudo mkdir /data/web_static; fi
+if [ ! -d /data/web_static/releases ]; then sudo mkdir /data/web_static/releases; fi
+if [ ! -d /data/web_static/shared ]; then sudo mkdir /data/web_static/shared; fi
+if [ ! -d '/data/web_static/releases/test' ]; then sudo mkdir /data/web_static/releases/test; fi
+if [ ! -f /data/web_static/releases/test/index.html ]; then sudo touch /data/web_static/releases/test/index.html; fi
+echo -e \<html\>\\n\\t\<head\>\<\/head\>\\n\\t\<body\>\\n\\t\\tHi I am Ahmed\\n\\t\<\/body\>\\n\<\/html\> > '/data/web_static/releases/test/index.html'
+if [ -L /data/web_static/current ]; then sudo rm /data/web_static/current; else sudo ln -s '/data/web_static/releases/test' /data/web_static/current; fi
 sudo chown -R ubuntu:ubuntu /data/
 HBNB="server_name _;\n\tlocation \/hbnb_static\/ {\n\talias \/data\/web_static\/current;\n\t}"
 sudo sed -i "s/server_name _;/$HBNB/" "/etc/nginx/sites-available/default"
