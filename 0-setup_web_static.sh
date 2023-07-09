3dc1506d902e10ab490fa9cbf891d3090a5dd5f4
Ahmed Olalekan <ahmedolalekan2@gmail.com>
2023-07-09 23:02:56 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
-# this script sets up my web servers for the deployment of web_static
-
-sudo apt-get update
-sudo apt-get install nginx
-
-if [ ! -d /data ]; then sudo mkdir /data; fi
-if [ ! -d /data/web_static ]; then sudo mkdir /data/web_static; fi
-if [ ! -d /data/web_static/releases ]; then sudo mkdir /data/web_static/releases; fi
-if [ ! -d /data/web_static/shared ]; then sudo mkdir /data/web_static/shared; fi
-if [ ! -d '/data/web_static/releases/test' ]; then sudo mkdir /data/web_static/releases/test; fi
-if [ ! -f /data/web_static/releases/test/index.html ]; then sudo touch /data/web_static/releases/test/index.html; fi
-echo -e \<html\>\\n\\t\<head\>\<\/head\>\\n\\t\<body\>\\n\\t\\tHi I am Ahmed\\n\\t\<\/body\>\\n\<\/html\> > '/data/web_static/releases/test/index.html'
-if [ -L /data/web_static/current ]; then sudo rm /data/web_static/current; else sudo ln -s '/data/web_static/releases/test' /data/web_static/current; fi
-sudo chown -R ubuntu:ubuntu /data/
-HBNB="server_name _;\n\tlocation \/hbnb_static\/ {\n\talias \/data\/web_static\/current;\n\t}"
-sudo sed -i "s/server_name _;/$HBNB/" "/etc/nginx/sites-available/default"
-
-sudo service nginx restart
+# sets up web servers for the deployment of web_static
+apt-get update -y > /dev/null 2>&1
+apt-get install nginx -y > /dev/null 2>&1
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo "Hello from web_static" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/^\tserver_name.*/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart > /dev/null 2>&1
