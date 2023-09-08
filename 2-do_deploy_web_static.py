b01458f497458ddf1ded9fd12543643868ebc134
Huming-bird <ahmedolalekan2@gmail.com>
2023-09-07 23:44:17 -0700

    updates

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,17 +11,23 @@ env.user = 'ubuntu'
 def do_deploy(archive_path):
     """distributed the archive to my web servers"""
     if os.path.isfile(archive_path) is False:
+        print('i am here')
         return False
     ds = archive_path.split("/")[-1]
     if put(archive_path, "/tmp/{}".format(ds)).failed:
+        print('unsuccesful')
         return False
     name = ds.split(".")[0]
     if run("rm -rf /data/web_static/releases").failed:
         return False
     if run("mkdir -p /data/web_static/releases").failed:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-            format(ds, name)).failed:
+    if run("tar -xzvf /tmp/{} -C /data/web_static/releases".
+            format(ds)).failed:
+        return False
+    if run("mkdir /data/web_static/releases/{}".format(name)).failed:
+        return False
+    if run("mv /data/web_static/releases/web_static/* /data/web_static/releases/{}".format(name)).failed:
         return False
     if run("rm /tmp/{}".format(ds)).failed:
         return False
